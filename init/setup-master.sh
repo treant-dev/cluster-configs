75c973a729cad6d6d03efd40ea128690fd44f0f5
Konstantin Kostikov <kostikov.konstantin.a@gmail.com>
2024-10-20 17:31:02 +0200

    kubeadm update to ipv4

--- init/setup-master.sh
+++ init/setup-master.sh
@@ -5,3 +5,5 @@ sudo kubeadm init --config kubeadm-config.yaml
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+sudo kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/refs/tags/v3.28.2/manifests/calico.yaml
