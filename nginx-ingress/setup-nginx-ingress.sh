3e936b0b33f76bbca0ceeba9cbf80403d9eecf01
Konstantin Kostikov <kostikov.konstantin.a@gmail.com>
2024-11-17 20:43:45 +0100

    install ingress-nginx with loadbalancer and minio no tsl ingress added

--- nginx-ingress/setup-nginx-ingress.sh
+++ nginx-ingress/setup-nginx-ingress.sh
@@ -7,7 +7,6 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm install ingress-nginx ingress-nginx/ingress-nginx \
   --namespace ingress-nginx \
   --create-namespace \
-  --set controller.service.type=NodePort \
-  --set controller.service.nodePorts.http=30080 \
-  --set controller.service.nodePorts.https=30443
+  --set controller.service.type=LoadBalancer \
+  --set controller.service.externalIPs={167.235.105.188}
 
