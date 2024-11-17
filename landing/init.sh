9d8cf3911e462282bc2bbce543b924b6235bb575
Konstantin Kostikov <kostikov.konstantin.a@gmail.com>
2024-11-17 23:48:14 +0100

    landing deployment added + some fixes

---
+++ landing/init.sh
@@ -0,0 +1,4 @@
+kubectl create configmap landing-nginx-proxy --from-file=nginx.conf -n default
+kubectl apply -f landing-deployment.yaml
+kubectl apply -f landing-service.yaml
+kubectl apply -f landing-ingress.yaml
--- nginx-ingress/README.md
+++
@@ -1,2 +0,0 @@
-# Why? 
-Instead of `[setup-nginx-proxy-to-nodeport.sh](setup-nginx-proxy-to-nodeport.sh)` you can use iptables, but I fucked up with it
