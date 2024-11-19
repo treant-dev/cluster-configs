d5cd095e08d7f4e1368811626597bcb719d6d6b5
Konstantin Kostikov <kostikov.konstantin.a@gmail.com>
2024-11-19 14:43:12 +0100

    fix nginx

--- landing/init.sh
+++ landing/init.sh
@@ -1,3 +1,4 @@
+kubectl delete configmap landing-nginx-proxy -n default
 kubectl create configmap landing-nginx-proxy --from-file=nginx.conf -n default
 kubectl apply -f landing-deployment.yaml
 kubectl apply -f landing-service.yaml
